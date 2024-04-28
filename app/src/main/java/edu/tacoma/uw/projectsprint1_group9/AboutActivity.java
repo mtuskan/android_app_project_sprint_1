cef5102b2c1e9766397402e32814d93cf3743fbb
James <jjl336@uw.edu>
2024-04-27 23:35:02 -0700

    Changed app logo, added hyperlinks to emails, add button functionality, merged with enriqueBranch

--- app/src/main/java/edu/tacoma/uw/projectsprint1_group9/AboutActivity.java
+++ app/src/main/java/edu/tacoma/uw/projectsprint1_group9/AboutActivity.java
@@ -1,6 +1,8 @@
 package edu.tacoma.uw.projectsprint1_group9;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,23 @@ public class AboutActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        findViewById(R.id.ResourcesButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), resourcesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.HomeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/edu/tacoma/uw/projectsprint1_group9/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/projectsprint1_group9/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.AboutButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), AboutInformationActivity.class);
+                Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
                 startActivity(intent);
             }
         });
