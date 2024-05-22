f9d029e0697f701c29fac1f91f56544fb1ffa595
James <jjl336@uw.edu>
2024-05-22 08:20:27 -0700

    Added Photos

--- app/src/main/java/edu/tacoma/uw/projectsprint1_group9/StudyFragment.java
+++ app/src/main/java/edu/tacoma/uw/projectsprint1_group9/StudyFragment.java
@@ -1,22 +1,20 @@
 package edu.tacoma.uw.projectsprint1_group9;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class StudyFragment extends Fragment {
+class StudyFragment extends Fragment {
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_study, container, false);
     }
 }
