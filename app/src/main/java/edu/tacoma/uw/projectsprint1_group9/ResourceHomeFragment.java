da64786e4059546660299f6966d4cf837e4a81dd
James <jjl336@uw.edu>
2024-05-23 18:03:09 -0700

    Added Fragments versions of activities. Navigation bar has functionality

--- app/src/main/java/edu/tacoma/uw/projectsprint1_group9/ResourceHomeFragment.java
+++ app/src/main/java/edu/tacoma/uw/projectsprint1_group9/ResourceHomeFragment.java
@@ -11,12 +11,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ResouceHomeFragment extends Fragment {
+public class ResourceHomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resouce_home, container, false);
+        return inflater.inflate(R.layout.fragment_resource_home, container, false);
     }
 }
